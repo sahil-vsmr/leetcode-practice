11e74534a871885afa170d9008a5e1a87d6241a8
Sahil Agarwal <sahil.vsmr@gmail.com>
2022-08-17 10:21:01 +0530

    Time: 2 ms (73.04%), Space: 43.3 MB (47.14%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.java
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] shortestToChar(String s, char c) {
         int[] answer = new int[s.length()];
         
-        List<Integer> positions = new ArrayList<>();
+        /*List<Integer> positions = new ArrayList<>();
         
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == c) {
@@ -20,6 +20,37 @@ class Solution {
                 min = min > Math.abs(position - i) ? Math.abs(position - i) : min;
             }
             answer[i] = min;
+        }*/
+        
+        int[] leftTraverse = new int[s.length()];
+        int[] rightTraverse = new int[s.length()];
+        
+        int currentPosition = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                currentPosition = i;
+            }
+            leftTraverse[i] = currentPosition;
+        }
+        
+        currentPosition = -1;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                currentPosition = i;
+            }
+            rightTraverse[i] = currentPosition;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (leftTraverse[i] == -1) {
+                answer[i] = Math.abs(i - rightTraverse[i]);
+            } else if (rightTraverse[i] == -1) {
+                answer[i] = Math.abs(i - leftTraverse[i]);
+            } else {
+                answer[i] = Math.min(Math.abs(i - leftTraverse[i]), Math.abs(i - rightTraverse[i]));
+            }
         }
         return answer;
     }
