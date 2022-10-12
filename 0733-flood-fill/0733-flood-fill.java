44254658cd2ea9959a549be1e56cc96651296561
Sahil Agarwal <sahil.vsmr@gmail.com>
2022-10-12 21:17:17 +0530

    Time: 1 ms (91.00%), Space: 48.1 MB (47.06%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int orig = image[sr][sc];
+        int x = sr;
+        int y = sc;
+        
+        int xLength = image.length;
+        int yLength = image[0].length;
+        //System.out.println(image[x+1][y]);
+        //System.out.println(image[x][y]);
+        if (image[x][y] != color) {
+            image[x][y] = color;
+            if (x + 1 < xLength && image[x+1][y] == orig) {
+                image = floodFill(image, x+1, y, color);   
+            }
+
+            if (x - 1 >= 0 && image[x-1][y] == orig) {
+                image = floodFill(image, x-1, y, color);  
+            }
+
+
+            if (y + 1 < yLength  && image[x][y+1] == orig) {
+                image = floodFill(image, x, y+1, color);   
+            }
+
+
+            if (y - 1 >= 0 && image[x][y-1] == orig) {
+                image = floodFill(image, x, y-1, color); 
+            }
+        }
+        return image;
+    }
+}
