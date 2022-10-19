d981a1df03cc09026393481195a28cac5edbacc5
Sahil Agarwal <sahil.vsmr@gmail.com>
2022-10-19 09:34:30 +0530

    Time: 20 ms (51.50%), Space: 56.5 MB (85.16%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int prev = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (prev == nums[i]) {
+                return true;
+            }
+            prev = nums[i];
+        }
+        return false;
+    }
+}
